c8ddb69101839fc40ddc29e55e6caab2aaebcdb1
sachitshetty85 <162810297+sachitshetty85@users.noreply.github.com>
2024-03-09 19:22:04 +0530

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,44 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68SruYzTsou":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var myCode = function() {
+	$(document).on("keydown", function(e) {
+    if (e.keyCode == 9) {
+        e.preventDefault();
+        return false;
+    }
+	})
+}
+if (window.$ != null) { // If jQuery has already loaded, run myCode.
+
+	myCode();
+
+} else { // Else, load jQuery and then run myCode.
+
+	var jQueryLoader = document.createElement("script");
+
+	jQueryLoader.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+	
+
+	jQueryLoader.onload = function() {
+
+		jQueryLoader.onload = null;
+
+		myCode();
+
+	}
+
+	document.head.appendChild(jQueryLoader);
+
+}
+}
+
