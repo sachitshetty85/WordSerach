378e9f88a91d893e67219b9bc775ef7f00ed6224
sachitshetty85 <162810297+sachitshetty85@users.noreply.github.com>
2024-03-12 12:13:27 +0530

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,13 +2,50 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6ed9tUqhHj1":
+      case "5WOngKbXebe":
         Script1();
+        break;
+      case "664UKjQifmd":
+        Script2();
         break;
   }
 }
 
 function Script1()
+{
+  var myCode = function() {
+	$(document).on("keydown", function(e) {
+    if (e.keyCode == 9) {
+        e.preventDefault();
+        return false;
+    }
+	})
+}
+if (window.$ != null) { // If jQuery has already loaded, run myCode.
+
+	myCode();
+
+} else { // Else, load jQuery and then run myCode.
+
+	var jQueryLoader = document.createElement("script");
+
+	jQueryLoader.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js";
+	
+
+	jQueryLoader.onload = function() {
+
+		jQueryLoader.onload = null;
+
+		myCode();
+
+	}
+
+	document.head.appendChild(jQueryLoader);
+
+}
+}
+
+function Script2()
 {
   var myCode = function() {
 	$(document).on("keydown", function(e) {
